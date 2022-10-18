2fdbec44e0eb5afbac29e3e47842a4d89e1f8a0c
denis0108 <71262767+denis0108@users.noreply.github.com>
2022-10-18 15:39:30 +0300

    add arrays

---
+++ array.js
@@ -0,0 +1,66 @@
+"use strict"
+
+let arr1 = new Array()
+let arr2 = []
+
+let arr = ["kek", "lol"]
+
+console.log(arr.length)
+
+arr[1] = "orbidol"
+
+
+arr.push("mem")
+arr.unshift("mem2")
+
+console.log(arr)
+
+let fruits = []; // создаём массив
+
+fruits[99999] = 5; // создаём свойство с индексом, намного превышающим длину массива
+
+fruits.push("jopa")
+
+fruits.age = 25;
+
+console.log(fruits)
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(i)
+}
+
+for(let key in arr) {
+    console.log(key)
+}
+
+let array = [1, 2, 3, 4, 5, 6]
+
+array.length = 3
+
+console.log(array)
+
+let styles = ["Джаз", "Блюз"]
+
+styles.push("Рокк-н-Ролл")
+
+styles[(styles.length - 1)/2] = "Классика"
+
+alert(styles.shift())
+
+styles.unshift("Реп", "Регги")
+
+console.log(styles)
+
+function getMaxSubSum(arr) {
+    let sum = 0
+    let maxSum = 0
+    let partialSum = 0
+    for (let item of arr) {
+        partialSum += item;
+        maxSum = Math.max(maxSum, partialSum);
+        if (partialSum < 0) partialSum = 0;
+    }
+
+    return maxSum;
+}
+
